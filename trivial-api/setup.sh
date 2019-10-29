3fcb4f41e4faaa77260866e219f37f40ddbf88ab
Fred T. Dunaway <fred.t.dunaway@gmail.com>
2019-10-29 18:51:22 -0400

    Fixing setup script

--- trivial-api/setup.sh
+++ trivial-api/setup.sh
@@ -5,13 +5,13 @@ sudo apt-get install certbot --yes
 sudo apt-get install certbot python-certbot-nginx --yes
 echo '---> Installing pip <----------'
 sudo apt-get install pip --yes
-#echo '---> Installing git <----------'
-#sudo apt-get install git --yes
+echo '---> Installing git <----------'
+sudo apt-get install git --yes
 sudo mkdir /var/www/html/.well-known
 sudo mkdir /var/www/html/.well-known/acme-challenge
-#echo 'Cloning trivial REST API'
-#git git clone https://github.com/mrncmoose/trivialflaskapi
-cd ~/trivialflaskapi/trivial-api
+echo 'Cloning trivial REST API'
+git clone https://github.com/mrncmoose/trivial_flask_api.git
+cd ~/trivial_flask_api/trivial-api
 echo '---> Installing Python packages <----------'
 pip install -r requirements.txt
 chmod 775 *.sh
