0ab7afd92c414c1726134988ab475ef7d93518fe
Fred T. Dunaway <fred.t.dunaway@gmail.com>
2019-10-29 18:49:03 -0400

    Adding setup script

---
+++ trivial-api/setup.sh
@@ -0,0 +1,17 @@
+echo '---> Installing Nginx <----------'
+sudo apt-get install nginx --yes
+echo '---> Installing certbot <----------'
+sudo apt-get install certbot --yes
+sudo apt-get install certbot python-certbot-nginx --yes
+echo '---> Installing pip <----------'
+sudo apt-get install pip --yes
+#echo '---> Installing git <----------'
+#sudo apt-get install git --yes
+sudo mkdir /var/www/html/.well-known
+sudo mkdir /var/www/html/.well-known/acme-challenge
+#echo 'Cloning trivial REST API'
+#git git clone https://github.com/mrncmoose/trivialflaskapi
+cd ~/trivialflaskapi/trivial-api
+echo '---> Installing Python packages <----------'
+pip install -r requirements.txt
+chmod 775 *.sh
